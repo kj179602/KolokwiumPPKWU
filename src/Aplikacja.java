6e82ccb49d4c82db013b5839b0b793addf84fa89
Katarzyna <kaj915@wp.pl>
2015-12-01 13:32:24 +0100

    dodanie secondLibrary

--- src/Aplikacja.java
+++ src/Aplikacja.java
@@ -1,10 +1,21 @@
 
 import java.io.File;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import bin.secondLibrary.Crypt;
+
+
 
 public class Aplikacja {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, IOException {
 		// TODO Auto-generated method stub
 		String prawidloweHaslo ="qwerty";
 		System.out.println("Podaj sciezke do pliku");
@@ -17,7 +28,10 @@ public class Aplikacja {
 		Scanner s2 = new Scanner(System.in);
 		String haslo = s2.next();
 		if(prawidloweHaslo.equals(haslo)){
-			Crypt.szyfrowanie(plik, plik2);
+			
+				Crypt.szyfrowanie(plik, plik2);
+				System.out.println("Plik zaszyfrowano");
+			
 		}
 		else{
 			System.out.println("Nieprawidlowe haslo!");
